18a94a043b5eb6e7c2c62f2f1a03ea6dd71478a5
sang <rkooo567@gmail.com>
2024-04-18 00:50:43 -0700

    ip

--- vllm/worker/model_runner.py
+++ vllm/worker/model_runner.py
@@ -34,8 +34,9 @@ LORA_WARMUP_RANK = 8
 _BATCH_SIZE_ALIGNMENT = 8
 # Capture graphs for token size 1, 2, 4, 8, 16, 24, 32, 40, ..., 256.
 # NOTE: _get_graph_batch_size needs to be updated if this list is changed.
+MAX_BATCH_SIZE = 256
 _BATCH_SIZES_TO_CAPTURE = [1, 2, 4] + [
-    _BATCH_SIZE_ALIGNMENT * i for i in range(1, 33)
+    _BATCH_SIZE_ALIGNMENT * i for i in range(1, MAX_BATCH_SIZE // _BATCH_SIZE_ALIGNMENT)
 ]
 
 
